891ce722afeccabc40549e4d55d4159f92f3c29c
Monitoreo Central <monitcentral@promart.pe>
2025-03-11 18:47:00 -0500

    spinner and last changes

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ function mostrarAmigos(){
     let lista = document.getElementById("listaAmigos");
     lista.innerHTML = "";
     listaAmigos.forEach((amigo) => {
-        let item = document.createElement("li");
+        let item = document.createElement("ul");
         item.textContent = amigo;
         lista.appendChild(item);
         
@@ -40,8 +40,36 @@ function sortearAmigo(){
         return;
     }
     let amigo = listaAmigos[Math.floor(Math.random() * listaAmigos.length)];
-    mostrarAmigos2("li", `El amigo secreto es: ${amigo}`);
-
+    mostrarAmigos2("ul", `El amigo secreto es: ${amigo}`);
+    limpiarLista();
     return amigo;
 }
 
+function limpiarLista() {
+    document.querySelector('#resultado').value = '';
+}
+
+
+/* spinnerFlanders */
+const progress = document.querySelector('.progress');
+const audio = document.getElementById('loading-audio');
+const loadingText = document.querySelector('.loading-text');
+
+document.addEventListener('click', async () => {
+    try {
+        await audio.play(); // Intentar reproducir el audio
+        
+        // Cambiar el mensaje y empezar la animación de carga
+        loadingText.textContent = "Loading...";
+        progress.style.width = '100%';
+
+        // Redirigir después de que el audio comience a reproducirse
+        setTimeout(() => {
+            window.location.href = 'Home.html';
+        }, 9000);
+        
+    } catch (error) {
+        console.error("Error al reproducir el audio:", error);
+        loadingText.textContent = "Por favor, permite la reproducción de audio.";
+    }
+}, { once: true }); // Ejecutar solo una vez
